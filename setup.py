fb08ceb829545a54b2b3afd1ca6c04b42d7f5820
Alexander Lenail <zfrenchee@gmail.com>
2018-09-18 11:44:46 -0400

    Quick re-organization

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='axial',
+    packages=['axial'],
+    package_data={'axial': []},
+    version='0.0.1',
+    url='https://github.com/zfrenchee/axial',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'],
+    license='MIT',
+    author='zfrenchee',
+    author_email='alex@lenail.org',
+    description='',
+    install_requires=[],
+)
+
